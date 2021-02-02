cd188cb81695d7623012f8a8903d23e214529c4a
samiksha bhasin <samiksha.bhasin@outlook.com>
2021-02-01 18:43:23 -0800

    added constraint

--- sketch.js
+++ sketch.js
@@ -1,7 +1,8 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
-
+const Constraint = Matter.Constraint;
+var cLog;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
@@ -34,8 +35,11 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
+    cLog = new Log(230, 180, 80, PI/2);
+
     bird = new Bird(100,100);
 
+    chain = new Chain(bird.body, cLog.body);
 }
 
 function draw(){
@@ -59,6 +63,9 @@ function draw(){
     log4.display();
     log5.display();
 
+    cLog.display();
     bird.display();
+    chain.display();
     platform.display();
+
 }
